3ff26f7c061cbfb420fad986b0ebb4326c318cdf
iraritchiemeek <apprentice@StMac4.local>
2015-05-02 20:49:15 +1200

    Test editing todo lists

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  RSpec.configure do |c|
+    c.expose_current_running_example_as :example
+  end
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
